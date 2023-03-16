812a264337b91e742f9c96502581c3dab0e7e82d
BigShot09 <khotso.makara09>
2023-03-16 15:17:06 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line with the usage of puts funtcion
+ * Return: 0 (the project will be a success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
