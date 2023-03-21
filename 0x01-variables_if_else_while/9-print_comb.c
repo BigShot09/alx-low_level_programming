98e33a882efcaa907d91ca130da023b094251d85
BigShot09 <khotso.makara09>
2023-03-20 19:58:15 -0700

    9-print_comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,14 +12,13 @@ int main(void)
 
 	for (b = 0 ; b < 10 ; b++)
 	{
-		if (b == 9)
-			putchar(b + '0');
-		else
+		putchar(b + '0');
+		if (b != 9)
 		{
-			putchar(b + '0');
 			putchar(',');
 			putchar(' ');
 		}
 	}
+	putchar('\n');
 	return (0);
 }
