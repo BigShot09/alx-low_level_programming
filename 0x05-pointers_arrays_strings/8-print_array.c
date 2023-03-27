4d4a9ed1145db75f55e78b6a0bb9262864145241
BigShot09 <khotso.makara09>
2023-03-27 14:28:33 -0700

    8-print_array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,7 +12,7 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0 ; i < n ; i++)
+	for (i = 0 ; i < (n - 1) ; i++)
 	{
 		printf("%d, ", a[i]);
 	}
@@ -20,5 +20,5 @@ void print_array(int *a, int n)
 		{
 			printf("%d", a[n - 1]);
 		}
-		printf("\n");
+			printf("\n");
 }
