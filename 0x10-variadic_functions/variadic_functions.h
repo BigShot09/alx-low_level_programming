a1149ec3b4c7dbce0eee3e7abac7da6ed1d30fc9
BigShot09 <khotso.makara09>
2023-04-20 09:53:37 -0700

    0-sum_them_all.c

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,5 @@
-#ifndef VARIADIC_FUNCTIONS.H
-#define VARIADIC_FUNCTIONS.H
-
+#ifndef VARIADIC_H_
+#define VARIADIC_H_
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
